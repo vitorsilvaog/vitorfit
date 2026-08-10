b44118c0de8a08b61ca0c4625fa6ee25ad0885a7
Joao Vitor Silva <victorsilvaog02@gmail.com>
2026-08-10 22:03:09 +0200

    Convertir VitorFit en PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import RegisterSW from "./register-sw";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
